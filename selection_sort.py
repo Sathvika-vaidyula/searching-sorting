368c8695fa2cacb322d85e74e4eca94b1e5eeeed
Sathvika-vaidyula <sathvika.vaidyula@gmail.com>
2026-06-22 17:18:26 +0530

    selection

---
+++ selection_sort.py
@@ -0,0 +1,16 @@
+def selection(arr):
+    size = len(arr)
+
+    for i in range(size - 1):
+        min_index = i
+
+        for j in range(i + 1, size):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+lst = [2, 4, 3, 6, 5]
+print(selection(lst))
